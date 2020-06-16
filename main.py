2a65a939f72bd43ca1042faa0d02065831efd0fb
Justin Bouscal <justin@A008207.local>
2020-06-15 18:28:26 -0700

    message

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ x = random.randint(0,10)
 
 if x < 5:
     # indented four spaces
-    print("x is less than 5.")
+    print("x is less than 5, it is " x)
 else:
-    print("x is greater than 5")
+    print("x is greater than 5, is it "x)
